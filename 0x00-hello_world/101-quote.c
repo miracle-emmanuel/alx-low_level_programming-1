aa6826e975395681297775784c0472a8828df43c
jubelo <ohootuberecaiah@gmail.com>
2023-02-16 14:22:51 -0800

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unlisted.h>
+/**
+ * main - a program thats print a line to the standard e
+ * Return: 1 (sucess)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
