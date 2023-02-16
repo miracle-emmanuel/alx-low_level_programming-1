35a8985d38fe67f93095d6356072a2f9ffb5afc9
jubelo <ohootuberecaiah@gmail.com>
2023-02-16 14:28:06 -0800

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unlisted.h>
+#include <unistd.h>
 /**
  * main - a program thats print a line to the standard e
  * Return: 1 (sucess)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
