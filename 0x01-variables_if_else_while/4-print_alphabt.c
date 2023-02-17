100134c17b8c51f3a8ce978961599535de4b2d3b
jubelo <ohootuberecaiah@gmail.com>
2023-02-17 14:29:00 -0800

    no 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - removing q and e
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
