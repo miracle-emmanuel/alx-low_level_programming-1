789a91db9257a919c81a560a8e6d1cb58fd0e6a7
jubelo <ohootuberecaiah@gmail.com>
2023-02-21 14:27:56 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @c: value
+ * Return: abs value or number or zero
+ */
+int _abs(int)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
