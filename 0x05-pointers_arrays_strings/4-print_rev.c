d34fe6fd46f1cf3a86c6b5ed7c263250f1ddadbc
jubelo <ohootuberecaiah@gmail.com>
2023-02-27 08:22:18 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - Prints reverse
+ * @s: reverswee
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchatr('\n');
+}
