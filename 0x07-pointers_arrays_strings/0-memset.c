a00223743b0381b27d7d0c9abfde1a7143412288
jubelo <ohootuberecaiah@gmail.com>
2023-03-06 11:01:56 -0800

    number 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#includ "main.h"
+
+/**
+ * _memset - Entry point
+ * @s: pointed destinstion
+ * @b: constant
+ * @n: bytes
+ * Return: 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
