c3c6c9dc2ed7910f2b63e10d2280db0e8c725bfc
jubelo <ohootuberecaiah@gmail.com>
2023-03-06 12:06:50 -0800

    no 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - entry point
+ * @a: array
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int a, b;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 8; b++)
+			_putchar(a[i][n]);
+	}
+}
