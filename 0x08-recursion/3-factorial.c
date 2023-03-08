435aa587fa8aa13fb84aeec67f74ece96f3f57e3
jubelo <ohootuberecaiah@gmail.com>
2023-03-08 11:38:48 -0800

    no 4

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,13 +9,13 @@
 int factorial(int n)
 {
 	int f;
-
-	if (n <= 1)
+	
+	if (n < 0)
 	{
-		return (1);
-	}
-	else if (n < 0)
 		return (-1);
+	}
+	else if (n <= 1)
+		return (1);
 	f = n * factorial(n - 1);
 	return (f);
 }
