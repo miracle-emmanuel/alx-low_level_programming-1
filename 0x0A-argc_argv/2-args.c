d2d91b7968cbd17733761d5ad25a1631651cd1b7
jubelo <ohootuberecaiah@gmail.com>
2023-03-10 04:57:19 -0800

    no 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print arg content
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
