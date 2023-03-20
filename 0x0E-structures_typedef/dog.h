64e10e1f18dd2857477f0b3f20760bdba1068ad2
jubelo <ohootuberecaiah@gmail.com>
2023-03-20 05:28:56 -0700

    no 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,8 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
- * * struct dog - the dogs info
+ * struct dog - the dogs info
  * @name: n1
  * @age: n2
  * @owner: n3
@@ -26,7 +26,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
